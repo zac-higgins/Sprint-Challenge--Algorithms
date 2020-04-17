a0926de6616210968732f8896f4be8f7c254129a
Zac Higgins <zachiggins@Higgins-iMac.local>
2020-04-17 10:21:36 -0600

    completed assignment

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -93,33 +93,79 @@ class SortingRobot:
         return self._light == "ON"
 
     def sort(self):
+        # -------- Plan ------#
         """
-        Sort the robot's list.
+        1. Pick up first item
+        2. Move right
+        3. set light on
+        4. while light on
+            - set light off
+            - while robot can move right
+                - if item held is greater than item in front of robot:
+                    - swap item
+                    - move right
+                    - set light on
+                - otherwise, move right without swapping
+            - Robot will eventually get to the far right side
+            - begin comparisons while iterating left until you reach the 'none' spot (empty spot)
+            - if the item held is greater than the item in front of the robot, 
+                -swap
+                -move left
+                - turn light on
+            - otherwise, move left without swapping
+            - once you reach the empty spot AND you're not at the far right,
+                - swap
+                - move right
+                - swap
+                - move right
+            - if light is still on, repeat from step 4
+        5. swap with the empty spot in front of you
+
+        Sorted
         """
-        # turn light on
+        # -------- / Plan ------#
+        # robot starts at position 0,
+        # initial pickup and move to position 1, sets light on to trigger while loop
+        self.swap_item()
+        self.move_right()
         self.set_light_on()
-        # while light is on
         while self.light_is_on():
-        #   turn light off
+            # set light off to allow while loop to close when complete
             self.set_light_off()
-        #   for loop through the list
-            for i in range(1, len(self._list) -1):
-                #Pick up first item in list
+            # while loop to catch when robot hits empty slot on the way back left
+            while self.compare_item() is not None:
+                #while loop to catch when robot has hit the far right spot
+                while self.can_move_right():
+                    # if item held is greater than item in spot, swap and move right, light on to stay in outer while loop
+                    if self.compare_item() == 1:
+                        self.swap_item()
+                        self.move_right()
+                        self.set_light_on()
+                    # otherwise, move right without swapping. light is still off
+                    else:
+                        self.move_right()
+                # while loop to move left until robot hits the empty spot
+                while self.can_move_left() and self.compare_item() is not None:
+                    # while iterating left, if item held is greater than item in current spot,
+                    #  swap, move left, set light on
+                    if self.compare_item() == 1:
+                        self.swap_item()
+                        self.move_left()
+                        self.set_light_on()
+                    # otherwise, move left without swapping
+                    else:
+                        self.move_left()
+            # once the empty spot is reached, prior while loop is exited
+            # if robot is not at the far right, swap with the empty spot, move right, pick up item, move right
+            if self.can_move_right():
                 self.swap_item()
-                #move right
                 self.move_right()
-                #compare
-                if self.compare_item() == 1:
-                    #swap
-                    self.swap_item()
-                    #turn light on
-                    self.set_light_on
-                elif self.compare_item() is None:
-                    # self.swap_item()
-                    self.set_light_off()
-        # ------ basically trying to bubble sort but with the robot commands, wasn't able to get it -----#
-
-
+                self.swap_item()
+                self.move_right()
+            # if the light is still on the list is not sorted yet and the loop will repeat
+        # otherwise, you will be at the last empty spot and drop the item held
+        self.swap_item()
+        # list will be sorted
         return
 
 
@@ -128,7 +174,7 @@ if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28]
+    l = [10, 4, 3, 2, 5, 6]
     # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
